67a88c0b471d5da4177bbd7f1a7191f9595a5390
Gareth Brown <gaz@gbmail.biz>
2018-01-13 21:32:57 +0000

    tidy up install

--- files/bin/compile.sh
+++ files/bin/compile.sh
@@ -17,4 +17,19 @@ cd /tmp/httpd-${HTTPD_VER}
         --with-included-apr \
         --with-port=${PROXY_PORT} && \
     make && \
-    make install
+    make install && \
+    make clean
+
+apt-get purge libpcre3-dev \
+    zlib1g-dev \
+    build-essential \
+    libapr1-dev \
+    libaprutil1-dev
+apt-get autoremove
+apt-get clean
+
+rm -rf /tmp/httpd-${HTTPD_VER} \
+    /var/lib/apt/lists/* \
+    /tmp/httpd-${HTTPD_VER}.tar.gz \
+    /tmp/apr-1.6.3.tar.gz \
+    /tmp/apr-util-1.6.1.tar.gz
